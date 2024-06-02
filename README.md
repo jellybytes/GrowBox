3564df00d1dd6ad436f3b966151c4884f65e42bc
jellybytes <simon.ruhnau@protonmail.com>
2024-06-02 17:28:42 +0200

    Added OLED ssd1306 driver

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Growbox software raspberry pi. Read temperature sensors, control fans and lights
 # Temperature sensors
 
 2 x Bosch BME280
-i2C Adresses 0x76 / 0x77 -> solder bridge + cut default bridge!!
+i2C Adresses 0x76 / 0x77 -> connect solder bridge + cut default bridge!!
 
-## Bring up:
+**Bring up:**
 - enable i2c raspberry pi
 - `sudo apt install i2c-tools`
 - `i2cdetect -y 1` on raspi command line to verify
--- software/src/main.c
+++ software/src/main.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "bme280.h"
 #include <wiringPi.h>
+#include "ssd1306_i2c.h"
+#include "bme280.h"
 
 #define BME280_IN_ADDR 	0x76
 #define BME280_OUT_ADDR 0x77
 
 void printBME280();
+void testOLED();
 
 int main()
 {
 	printBME280();
+	testOLED();
+
     return 0;
 }
+
+void testOLED()
+{
+	ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
+
+	ssd1306_display(); //Adafruit logo is visible
+	ssd1306_clearDisplay();
+	delay(5000);
+
+	char* text = "This is demo for SSD1306 i2c driver for Raspberry Pi";
+	ssd1306_drawString(text);
+	ssd1306_display();
+	delay(5000);
+
+	ssd1306_dim(1);
+	ssd1306_startscrollright(00,0xFF);
+	delay(5000);
+
+	ssd1306_clearDisplay();
+	ssd1306_fillRect(10,10, 50, 20, WHITE);
+	ssd1306_fillRect(80, 10, 130, 50, WHITE);
+	ssd1306_display();
+}
+
 
 void printBME280()
 {
