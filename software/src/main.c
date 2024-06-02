dcf4962328e9d67b7a3879b09e65915d18922392
jellybytes <simon.ruhnau@protonmail.com>
2024-06-02 18:20:07 +0200

    Print BME280 data OLED display

--- software/src/main.c
+++ software/src/main.c
@@ -11,6 +11,9 @@
 void printBME280();
 void testOLED();
 
+BME280 bme280_in = {0};
+BME280 bme280_out = {0};
+
 int main()
 {
 	printBME280();
@@ -22,32 +25,29 @@ int main()
 void testOLED()
 {
 	ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
-
-	ssd1306_display(); //Adafruit logo is visible
+	// ssd1306_display(); //Adafruit logo is visible
 	ssd1306_clearDisplay();
-	delay(5000);
+	ssd1306_setTextSize(2);
 
-	char* text = "This is demo for SSD1306 i2c driver for Raspberry Pi";
-	ssd1306_drawString(text);
-	ssd1306_display();
-	delay(5000);
+	char print_buf[32] = {0};
+    sprintf(print_buf, "%.2f %cC\n", bme280_in.temperature, 0xF8);
+	ssd1306_drawString(print_buf);
 
-	ssd1306_dim(1);
-	ssd1306_startscrollright(00,0xFF);
-	delay(5000);
+	char print_buf1[32] = {0};
+    sprintf(print_buf1, "%.0f    %%\n", bme280_in.humidity);
+	ssd1306_drawString(print_buf1);
 
-	ssd1306_clearDisplay();
-	ssd1306_fillRect(10,10, 50, 20, WHITE);
-	ssd1306_fillRect(80, 10, 130, 50, WHITE);
-	ssd1306_display();
+	char print_buf2[32] = {0};
+    sprintf(print_buf2, "%.0f  hPa\n", bme280_in.pressure);
+	ssd1306_drawString(print_buf2);
+
+	// ssd1306_display();
 }
 
 
 void printBME280()
 {
     // holds calibration and values after read
-    BME280 bme280_in = {0};
-    BME280 bme280_out = {0};
 
     bme280_in.fd = wiringPiI2CSetup(BME280_IN_ADDR);
     if(bme280_in.fd < 0) {
@@ -57,9 +57,9 @@ void printBME280()
     setupBME280(&bme280_in);
     readBME280(&bme280_in);
 
-    printf("temperature: %f\n", bme280_in.temperature);
-    printf("humidity: %f\n", bme280_in.humidity);
-    printf("pressure: %f\n", bme280_in.pressure);
+    printf("temperature: %.2f °C\n", bme280_in.temperature);
+    printf("humidity: %.0f %%\n", bme280_in.humidity);
+    printf("pressure: %.0f hPa\n", bme280_in.pressure);
 
     bme280_out.fd = wiringPiI2CSetup(BME280_OUT_ADDR);
     if(bme280_out.fd < 0) {
