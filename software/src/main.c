6a0affb116a86405cabacc63ef6818d88454d710
jellybytes <simon.ruhnau@protonmail.com>
2024-06-02 17:17:11 +0200

    First test BME280 temperature sensors

--- software/src/main.c
+++ software/src/main.c
@@ -2,25 +2,47 @@
 #include <stdlib.h>
 
 #include "bme280.h"
-
 #include <wiringPi.h>
+
+#define BME280_IN_ADDR 	0x76
+#define BME280_OUT_ADDR 0x77
+
+void printBME280();
 
 int main()
 {
+	printBME280();
+    return 0;
+}
+
+void printBME280()
+{
     // holds calibration and values after read
-    BME280 bme280 = {0};
-    bme280.fd = wiringPiI2CSetup(BME280_ADDRESS);
-    if(bme280.fd < 0) {
-      printf("Device not found");
-      return -1;
+    BME280 bme280_in = {0};
+    BME280 bme280_out = {0};
+
+    bme280_in.fd = wiringPiI2CSetup(BME280_IN_ADDR);
+    if(bme280_in.fd < 0) {
+    	printf("Device not found");
+      	// return -1;
+	}
+    setupBME280(&bme280_in);
+    readBME280(&bme280_in);
+
+    printf("temperature: %f\n", bme280_in.temperature);
+    printf("humidity: %f\n", bme280_in.humidity);
+    printf("pressure: %f\n", bme280_in.pressure);
+
+    bme280_out.fd = wiringPiI2CSetup(BME280_OUT_ADDR);
+    if(bme280_out.fd < 0) {
+    	printf("Device not found");
+    	// return -1;
     }
 
-    setupBME280(&bme280);
-    readBME280(&bme280);
+    setupBME280(&bme280_out);
+    readBME280(&bme280_out);
 
-    printf("temperature: %f\n", bme280.temperature);
-    printf("humidity: %f\n", bme280.humidity);
-    printf("pressure: %f\n", bme280.pressure);
-
-    return 0;
+    printf("temperature: %f\n", bme280_out.temperature);
+    printf("humidity: %f\n", bme280_out.humidity);
+    printf("pressure: %f\n", bme280_out.pressure);
 }
