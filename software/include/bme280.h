e0db4ca135042f64a2ba449432948abe86fb3e8e
jellybytes <simon.ruhnau@protonmail.com>
2024-06-02 11:12:47 +0200

    Rearranged project structure

--- software/include/bme280.h
+++ software/include/bme280.h
@@ -50,26 +50,26 @@
 */
 typedef struct
 {
-  uint16_t dig_T1;
-  int16_t  dig_T2;
-  int16_t  dig_T3;
+  	uint16_t dig_T1;
+  	int16_t  dig_T2;
+  	int16_t  dig_T3;
 
-  uint16_t dig_P1;
-  int16_t  dig_P2;
-  int16_t  dig_P3;
-  int16_t  dig_P4;
-  int16_t  dig_P5;
-  int16_t  dig_P6;
-  int16_t  dig_P7;
-  int16_t  dig_P8;
-  int16_t  dig_P9;
+  	uint16_t dig_P1;
+  	int16_t  dig_P2;
+  	int16_t  dig_P3;
+  	int16_t  dig_P4;
+  	int16_t  dig_P5;
+  	int16_t  dig_P6;
+  	int16_t  dig_P7;
+  	int16_t  dig_P8;
+  	int16_t  dig_P9;
 
-  uint8_t  dig_H1;
-  int16_t  dig_H2;
-  uint8_t  dig_H3;
-  int16_t  dig_H4;
-  int16_t  dig_H5;
-  int8_t   dig_H6;
+  	uint8_t  dig_H1;
+  	int16_t  dig_H2;
+  	uint8_t  dig_H3;
+  	int16_t  dig_H4;
+  	int16_t  dig_H5;
+  	int8_t   dig_H6;
 } bme280_calib_data;
 
 /*
@@ -77,31 +77,30 @@ typedef struct
 */
 typedef struct 
 {
-  uint8_t pmsb;
-  uint8_t plsb;
-  uint8_t pxsb;
+  	uint8_t pmsb;
+  	uint8_t plsb;
+  	uint8_t pxsb;
 
-  uint8_t tmsb;
-  uint8_t tlsb;
-  uint8_t txsb;
+  	uint8_t tmsb;
+  	uint8_t tlsb;
+  	uint8_t txsb;
 
-  uint8_t hmsb;
-  uint8_t hlsb;
+  	uint8_t hmsb;
+  	uint8_t hlsb;
 
-  uint32_t temperature;
-  uint32_t pressure;
-  uint32_t humidity;  
-
+  	uint32_t temperature;
+  	uint32_t pressure;
+  	uint32_t humidity;  
 } bme280_raw_data;
 
 struct _BME280
 {
-  int fd;
-  bme280_calib_data cal;
-  bme280_raw_data raw;
-  float temperature;
-  float humidity;
-  float pressure;
+  	int fd;
+  	bme280_calib_data cal;
+  	bme280_raw_data raw;
+  	float temperature;
+  	float humidity;
+  	float pressure;
 };
 typedef struct _BME280 BME280;
 
