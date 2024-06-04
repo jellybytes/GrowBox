efc13d970be76f5fb0ec578951e56ab016e9e804
jellybytes <simon.ruhnau@protonmail.com>
2024-06-04 21:31:21 +0200

    First PWM motor control test

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # GrowBox
 Growbox software raspberry pi. Read temperature sensors, control fans and lights.
 
-# Temperature sensors
-
+# Temperature Sensors
 2 x Bosch BME280
 i2C Adresses 0x76 / 0x77 -> connect solder bridge + cut default bridge!!
 
-**Bring up:**
+**Bring Up:**
 - enable i2c raspberry pi
+- `sudo apt-get install libi2c-dev`
 - `sudo apt install i2c-tools`
 - `i2cdetect -y 1` on raspi command line to verify
+
+default baudrate 100Kbps -> increase with `gpio load i2c 400` to 400Kbps
+
+# Fan Speed Control
+- `gpio readall` to print wPi <-> BMC pin mapping
+
+**PWM Pins**
+| BMC | PWM |
+|--------|------|
+| GPIO12 | PWM0 |
+| GPIO18 | PWM0 |
+| GPIO13 | PWM1 |
+| GPIO19 | PWM1 |
+
+Use BCM pins `12` and `13` -> wPi pins `26` and `23`
+
+# Display
+SSD1306 based 128x64 pixel OLED screen. I2c address `0x3c`
--- software/src/main.c
+++ software/src/main.c
@@ -10,16 +10,43 @@
 
 void printBME280();
 void testOLED();
+void startPWM(int dc);
 
 BME280 bme280_in = {0};
 BME280 bme280_out = {0};
+const int PWM_0 = 26;
+const int PWM_1 = 23;
 
-int main()
+int main(int argc, char ** argv)
 {
+	// printf("argv[1]: %s", argv[1]);
+	// long dc = strtol(argv[1], NULL, 10);
 	printBME280();
 	testOLED();
+	// startPWM((int) dc);
 
     return 0;
+}
+
+void startPWM(int dc)
+{
+	// initialise wiringPi with wPi numbering
+	if (wiringPiSetup() == -1) 
+		exit(1);
+
+	// fixed period for all Duty cycles
+	pinMode(PWM_0, PWM_OUTPUT);
+	pinMode(PWM_1, PWM_OUTPUT);
+
+	pwmSetMode(PWM_MODE_MS);
+	// init PWM pins
+	// clock divider 19.2 MHz / x
+	// pwmFreq = 19.2e6 / clkDiv / range
+	pwmSetClock(5);
+	pwmSetRange(192);
+	// set PWM
+	pwmWrite(PWM_0, 96);
+	pwmWrite(PWM_1, dc);
 }
 
 void testOLED()
@@ -40,8 +67,7 @@ void testOLED()
 	char print_buf2[32] = {0};
     sprintf(print_buf2, "%.0f  hPa\n", bme280_in.pressure);
 	ssd1306_drawString(print_buf2);
-
-	// ssd1306_display();
+	ssd1306_display(); // needed
 }
 
 
